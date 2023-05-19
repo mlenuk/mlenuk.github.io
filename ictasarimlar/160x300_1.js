6e58eaebee79d4e94d306319b14d7ca447cebcce
mlenuk <134087093+mlenuk@users.noreply.github.com>
2023-05-20 00:44:42 +0700

    Create 160x300_1.js

---
+++ ictasarimlar/160x300_1.js
@@ -0,0 +1,8 @@
+  atOptions = {
+		'key' : 'dfe64d21a2865b03631274ddfbbf429c',
+		'format' : 'iframe',
+		'height' : 300,
+		'width' : 160,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplaynetwork.com/dfe64d21a2865b03631274ddfbbf429c/invoke.js"></scr' + 'ipt>');
