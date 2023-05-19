6aa25dadac37734a5e95e0223ead2472237d00da
mlenuk <134087093+mlenuk@users.noreply.github.com>
2023-05-20 00:20:20 +0700

    Create 160x300_1.js

---
+++ canhoquan3/160x300_1.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : '7f37778eb92c3eb3661c0d4e565427de',
+		'format' : 'iframe',
+		'height' : 300,
+		'width' : 160,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplaynetwork.com/7f37778eb92c3eb3661c0d4e565427de/invoke.js"></scr' + 'ipt>');
