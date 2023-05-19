6dff58db552a128c311f18a00df8f79906d44b6b
mlenuk <134087093+mlenuk@users.noreply.github.com>
2023-05-20 00:48:52 +0700

    Create NativeBanner_1.js

---
+++ ictasarimlar/NativeBanner_1.js
@@ -0,0 +1,10 @@
+const js_thml = () => {
+	let code = "";
+	code += "<script async=\"async\" data-cfasync=\"false\" src=\"//pl19445323.highrevenuegate.com/6e014150b2ddb84f14671ed26d19d329/invoke.js\"><\\/script>\n";
+	code += "<div id=\"container-6e014150b2ddb84f14671ed26d19d329\"></div>\n";
+	return code;
+}
+
+
+document.getElementById("js_html").innerText = js_thml();
+document.getElementById("html-text").value = js_thml();
