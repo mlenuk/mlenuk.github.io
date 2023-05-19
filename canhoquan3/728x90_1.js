ff6dad6d84ed4170a4f1766a9ab428eab499cdd9
mlenuk <134087093+mlenuk@users.noreply.github.com>
2023-05-20 00:19:20 +0700

    Create 728x90_1.js

---
+++ canhoquan3/728x90_1.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : '23a82d925eace64391e67c06646a3d01',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplaynetwork.com/23a82d925eace64391e67c06646a3d01/invoke.js"></scr' + 'ipt>');
