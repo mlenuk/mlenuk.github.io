a6f08180d2c2760d4f91ba8ba13fc277ed74f3a9
mlenuk <134087093+mlenuk@users.noreply.github.com>
2023-05-20 01:00:19 +0700

    Create 300x250.js

---
+++ axhvq/300x250.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : '2f2cd4c04b0b336023f7481c876aa8ea',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplaynetwork.com/2f2cd4c04b0b336023f7481c876aa8ea/invoke.js"></scr' + 'ipt>');
