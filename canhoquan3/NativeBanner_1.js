74c4708f3813f8171d30b1fb483fc048b22c9359
mlenuk <134087093+mlenuk@users.noreply.github.com>
2023-05-20 02:36:34 +0700

    Update NativeBanner_1.js

--- canhoquan3/NativeBanner_1.js
+++ canhoquan3/NativeBanner_1.js
@@ -1,10 +1,2 @@
-const js_thml = () => {
-	let code = "";
-	code += "<script async=\"async\" data-cfasync=\"false\" src=\"//pl19445217.highrevenuegate.com/e68bfb2e199d5558b9dcd7bbd9bdc3d5/invoke.js\"><\\/script>\n";
-	code += "<div id=\"container-e68bfb2e199d5558b9dcd7bbd9bdc3d5\"></div>\n";
-	return code;
-}
-
-
-document.getElementById("js_html").innerText = js_thml();
-document.getElementById("html-text").value = js_thml();
+document.write('<script async="async" data-cfasync="false" src="//pl19445217.highrevenuegate.com/e68bfb2e199d5558b9dcd7bbd9bdc3d5/invoke.js"></script>');
+document.write('<div id="container-e68bfb2e199d5558b9dcd7bbd9bdc3d5"></div>');
