dc934e940168a61ae0cc80d28806d1f960867b8a
mlenuk <134087093+mlenuk@users.noreply.github.com>
2023-05-20 01:01:56 +0700

    Create 160x300.js

---
+++ axhvq/160x300.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : 'e6af82e63aef46653a6646d59706c1f3',
+		'format' : 'iframe',
+		'height' : 300,
+		'width' : 160,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplaynetwork.com/e6af82e63aef46653a6646d59706c1f3/invoke.js"></scr' + 'ipt>');
