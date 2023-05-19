73b1a299a766c436ef94d40aa9f94df63a110714
mlenuk <134087093+mlenuk@users.noreply.github.com>
2023-05-20 01:00:54 +0700

    Create 728x90.js

---
+++ axhvq/728x90.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : 'ea1f17d0b969f24752ab804615e84b6b',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplaynetwork.com/ea1f17d0b969f24752ab804615e84b6b/invoke.js"></scr' + 'ipt>');
