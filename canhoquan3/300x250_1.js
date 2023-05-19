38126a5bab052510582aa936645d8a2f102a5d2f
mlenuk <134087093+mlenuk@users.noreply.github.com>
2023-05-20 00:17:23 +0700

    Create 300x250_1.js

---
+++ canhoquan3/300x250_1.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : '62f6a8c3196370bd389a1644a3aa4214',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplaynetwork.com/62f6a8c3196370bd389a1644a3aa4214/invoke.js"></scr' + 'ipt>');
