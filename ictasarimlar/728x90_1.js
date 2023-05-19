f65615628c6e70e9d114aae46cfd8080bdccad8e
mlenuk <134087093+mlenuk@users.noreply.github.com>
2023-05-20 00:51:16 +0700

    Create 728x90_1.js

---
+++ ictasarimlar/728x90_1.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : '4d1f67861967e44b9f8336f8c607ee1a',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplaynetwork.com/4d1f67861967e44b9f8336f8c607ee1a/invoke.js"></scr' + 'ipt>');
