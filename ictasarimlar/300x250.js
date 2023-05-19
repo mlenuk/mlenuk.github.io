de9e9d966ad610a750d36045c170825dc3131357
mlenuk <134087093+mlenuk@users.noreply.github.com>
2023-05-20 00:42:44 +0700

    Create 300x250.js

---
+++ ictasarimlar/300x250.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : 'b68e9a7d927a0367bcd81b4d7760d29a',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplaynetwork.com/b68e9a7d927a0367bcd81b4d7760d29a/invoke.js"></scr' + 'ipt>');
