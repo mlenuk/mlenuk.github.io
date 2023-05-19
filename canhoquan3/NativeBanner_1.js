14606d8f1a25cd3233f4fbc2f146795b7792420a
mlenuk <134087093+mlenuk@users.noreply.github.com>
2023-05-20 00:37:55 +0700

    Create NativeBanner_1.js

---
+++ canhoquan3/NativeBanner_1.js
@@ -0,0 +1,10 @@
+const js_thml = () => {
+	let code = "";
+	code += "<script async=\"async\" data-cfasync=\"false\" src=\"//pl19445217.highrevenuegate.com/e68bfb2e199d5558b9dcd7bbd9bdc3d5/invoke.js\"><\\/script>\n";
+	code += "<div id=\"container-e68bfb2e199d5558b9dcd7bbd9bdc3d5\"></div>\n";
+	return code;
+}
+
+
+document.getElementById("js_html").innerText = js_thml();
+document.getElementById("html-text").value = js_thml();
