c98dd809d5e354c8340f986b95a96a75d823152e
mlenuk <134087093+mlenuk@users.noreply.github.com>
2023-05-20 02:35:20 +0700

    Update NatBan.js

--- axhvq/NatBan.js
+++ axhvq/NatBan.js
@@ -1,10 +1,2 @@
-const js_thml = () => {
-	let code = "";
-	code += "<script async=\"async\" data-cfasync=\"false\" src=\"//pl19445481.highrevenuegate.com/0b3eff38442f596c83c4b4cecd065489/invoke.js\"><\\/script>\n";
-	code += "<div id=\"container-0b3eff38442f596c83c4b4cecd065489\"></div>\n";
-	return code;
-}
-
-
-document.getElementById("js_html").innerText = js_thml();
-document.getElementById("html-text").value = js_thml();
+document.write('<script async="async" data-cfasync="false" src="//pl19445481.highrevenuegate.com/0b3eff38442f596c83c4b4cecd065489/invoke.js"></script>');
+document.write('<div id="container-0b3eff38442f596c83c4b4cecd065489"></div>');
